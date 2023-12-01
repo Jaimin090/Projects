ce7e4569a981bd5773722e37743a61b9b8faa4be
Jaimin Chavda <147872220+Novajaimin@users.noreply.github.com>
2023-12-01 13:59:43 -0700

    Delete HTML directory

--- HTML/Javascript/scripts/let_const.js
+++
@@ -1,5 +0,0 @@
-const RATE_OF_PAY = 75.0
-
-let salary = RATE_OF_PAY * hours
-
-const element = document.getElementById("myId")
--- HTML/Javascript/scripts/test-strings.js
+++
@@ -1,6 +0,0 @@
-let name = "Robin Suthar";
-console.log(name.length);
-console.log(name.toUpperCase());
-console.log(name.toLowerCase());
-console.log(name.charAt(6));
-console.log(name.substring(0,3));
